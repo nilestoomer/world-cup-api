e48ab670567e4dc45eff293e68ae90910ddf815e
nilestoomer <user@email.com>
2024-03-12 16:48:09 -0400

    updated for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
+const connectionString = process.env.DB_URL || "mongodb://127.0.0.1:27017/world_cup_api"
+
 mongoose.set("returnOriginal", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/world_cup_api").catch((err) => {
+mongoose.connect(connectionString).catch((err) => {
   console.log(`Error connection to MongoDB: ${err.message}`);
 });
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import logger from "morgan";
 import chalk from "chalk";
 
 const app = express();
-const PORT = 3017;
+const PORT = proccess.env.PORT || 3017;
 
 app.use(express.json());
 app.use(cors());
